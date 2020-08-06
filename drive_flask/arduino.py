21c0d180545405a59854ce4b1d34bfe2c1dd5113
orangepi_2106 <cot1884@gmail.com>
2020-08-06 12:13:26 +0500

    decode

--- drive_flask/arduino.py
+++ drive_flask/arduino.py
@@ -13,20 +13,36 @@ class Arduino():
         '/dev/ttyUSB4',
     ]
 
+    DECODE = {
+        "cmd" : {
+            "1001": "TEST",
+            "1": "FORWARD",
+            "2": "BACK",
+            "3": "LEFT",
+            "4": "RIGHT",
+            "5": "ON_LINE",
+            "0": "STOP",
+        },
+        "state": {
+            0: "ON",
+            1: "OFF",
+        },
+    }
+
     COMMAND_KEY_ORDER  = ["cmd", "lft_pwm", "rgt_pwm", ]
     RESPONCE_KEY_ORDER = [
-        (str, "last_cmd"), 
-        (int, "lft_pwm"), 
-        (int, "rgt_pwm"), 
-        (int, "lft_avg"), 
-        (int, "rgt_avg"), 
-        (float, "a0"), 
-        (float, "a1"), 
-        (float, "a2"), 
-        (float, "VCC"), 
-        (float, "I"), 
-        (int, "motors_state"), 
-        (int, "sensor_state"), 
+        (str, "last_cmd", "cmd"), 
+        (int, "lft_pwm", 0), 
+        (int, "rgt_pwm", 0), 
+        (int, "lft_avg", 0), 
+        (int, "rgt_avg", 0), 
+        (float, "a0", 0), 
+        (float, "a1", 0), 
+        (float, "a2", 0), 
+        (float, "VCC", 0), 
+        (float, "I", 0), 
+        (int, "motors_state", "state"),
+        (int, "sensor_state", "state"),
     ]
     TEST = 1001
     FORWARD = 1
@@ -54,7 +70,7 @@ class Arduino():
         try:
             self.port.close()
         except AttributeError:
-            print('Can not close port %s' % self.port, file = sys.stderr)        
+            print('Can not close port %s' % self.port, file = sys.stderr)
 
     def __str__(self):
         return str(self.port)
@@ -104,10 +120,13 @@ class Arduino():
     def __read_responce_dict(self):
         rez = {}
         try:
-            for _type, key in self.RESPONCE_KEY_ORDER:
+            for _type, key, f in self.RESPONCE_KEY_ORDER:
                 x = self.port.readline().decode().strip()
                 x = _type(x)
-                rez.update({key:x})
+                try:
+                    rez.update({key:self.DECODE[f][x]})
+                except KeyError:
+                    rez.update({key:x})
         except ValueError:
             rez = None
         return rez
@@ -115,8 +134,9 @@ class Arduino():
 if __name__ == '__main__':
     from pprint import pprint
     ino = Arduino()
-    pprint(ino.test())
-    sleep(1)
+    for i in range(10):
+        pprint(ino.test())
+        sleep(1)
     pprint(ino.forward(20,20))
     for i in range(10):
         pprint(ino.test())
@@ -126,5 +146,7 @@ if __name__ == '__main__':
         pprint(ino.test())
         sleep(1)
     pprint(ino.stop())
-    pprint(ino.test())
+    for i in range(10):
+        pprint(ino.test())
+        sleep(1)
         
